9e10889a03a2695d4e48f807c92cd95f468ffc66
JoeyTanner <joey.tanner.music@gmail.com>
2026-03-29 23:28:00 -0700

    Added setting the wafer dimensions buttons and output labels, as well as the current dimension button and output label, which currently just have dummy implementations

--- testing/basic_pyside6_gui.py
+++ testing/basic_pyside6_gui.py
@@ -22,17 +22,32 @@ class MainWindow(QMainWindow):
 
         self.input_text = None
         self.output_label = QLabel("Input will appear here")
+        self.text_input = QLineEdit()
+        self.text_input.setPlaceholderText("Enter dummy integer input: ")  # Add placeholder text
+
         self.output_label_cur_pos = QLabel("Current position will appear here")
 
         button = QPushButton('Connect arduino')
         button.clicked.connect(self.try_motor_connection)
         button.setFixedSize(QSize(120, 50))
 
-        self.text_input = QLineEdit()
-        self.text_input.setPlaceholderText("Enter test/dummy integer input: ") # Add placeholder text
         self.submit_button = QPushButton("Submit")
         self.submit_button.clicked.connect(self.on_submit_clicked)
 
+        # Spacing distance
+        self.spacing_input = QLineEdit()
+        self.spacing_input.setPlaceholderText("Spacing distance (mm):")
+
+        # Stopping time
+        self.stop_time_input = QLineEdit()
+        self.stop_time_input.setPlaceholderText("Stopping time between images (ms):")
+
+        # Emergency stop
+        self.emergency_button = QPushButton("EMERGENCY STOP")
+        self.emergency_button.setStyleSheet("background-color: red; color: white; font-weight: bold;")
+        #self.emergency_button.clicked.connect(self.on_emergency_stop)
+
+        # Enter wafer dimensions
         self.wafer_len = QLineEdit()
         self.wafer_len.setPlaceholderText("Enter wafer length: ")  # Add placeholder text
         self.submit_wafer_len = QPushButton("Submit")
@@ -43,6 +58,7 @@ class MainWindow(QMainWindow):
         self.submit_wafer_width = QPushButton("Submit")
         self.submit_wafer_width.clicked.connect(self.on_submit_wafer_width)
 
+        # Get current position (NOT IMPLEMENTED)
         self.get_cur_pos = QPushButton("Get current position")
         self.get_cur_pos.clicked.connect(self.on_get_cur_pos)
 
@@ -51,12 +67,18 @@ class MainWindow(QMainWindow):
         layout.addWidget(self.text_input)
         layout.addWidget(self.submit_button)
         layout.addWidget(self.output_label)
+
+        layout.addWidget(self.spacing_input)
+        layout.addWidget(self.stop_time_input)
+
         layout.addWidget(self.wafer_len)
         layout.addWidget(self.submit_wafer_len)
         layout.addWidget(self.wafer_width)
         layout.addWidget(self.submit_wafer_width)
         layout.addWidget(self.get_cur_pos)
         layout.addWidget(self.output_label_cur_pos)
+
+        layout.addWidget(self.emergency_button)
 
         container = QWidget()
         container.setLayout(layout)
@@ -86,11 +108,18 @@ class MainWindow(QMainWindow):
         print('Requesting current position')
         self.output_label_cur_pos.setText("Current pos requested")
         # add serial code
+'''
+this is where you would implement the method to send a stop command to the arduino when the emergency button is clicked.
+u  need to define a method in your basic_arduino_connection class that sends the appropriate command to the arduino to stop it
+and then call that method here.
 
-    def get_integer(self):
+    def on_emergency_stop(self):
+    if self._connection:
+        self._connection.send_stop()  # define this in basic_arduino_connection
+    print("Emergency stop triggered")
+'''
+def get_integer(self):
         return self.input_text
-
-
 
 
 if __name__ == '__main__':
